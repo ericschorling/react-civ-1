8b73321a5e1fa23a02b6fef3233ef8d36f49b6c4
Eric Schorling <eric.schorling@yahoo.com>
2021-09-08 12:33:59 -0400

    set up node backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 
 import './App.css';
+import React, {useState, useEffect} from 'react'
 import GameBoard from './components/GameBoard';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { Home } from './components/Home';
 
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    fetch("/api")
+    .then((res)=> res.json())
+    .then((data)=> setData(data.message))
+  }, [])
+
+  
   return (
     <div className="App">
       
       <Router>
         <header>
           <h1> Foundation the Game!!!</h1>
+          <p>{!data ? "Loading..." : data}</p>
         </header>
         <main>
           <div>
